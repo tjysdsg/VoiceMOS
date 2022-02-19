b5c2515c876cbc96786c8733b25618e83c9099da
tjysdsg <tjy1018543509@163.com>
2022-02-19 23:27:47 +0800

    Use RNN as decoder, better result achieved

--- dku/run.sh
+++ dku/run.sh
@@ -2,7 +2,7 @@ python train.py \
   --dataset_name BVCC \
   --data_dir data/phase1_main/DATA \
   --spk_embed_dir data/spk_embed \
-  --config configs/SLDNet-ML_MobileNetV3_FFN_1e-3.yaml \
+  --config configs/SLDNet-ML_MobileNetV3_RNN_1e-3.yaml \
   --update_freq 2 --seed 2337 \
   --tag sldnet_ml \
   "$@"
