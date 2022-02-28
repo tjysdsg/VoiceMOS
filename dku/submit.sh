328dacdebbf3a74d7f4f2e7065137c265b6fe134
tjysdsg <tjy1018543509@163.com>
2022-02-28 10:49:02 +0800

    Test phase

--- dku/submit.sh
+++ dku/submit.sh
@@ -1,14 +1,16 @@
-ep=12000
+ep=18000
 tag=sldnet_ml
 mode=mean_listener
 dataset=BVCC
+phase=test
 
 python inference_for_voicemos.py \
   --dataset_name ${dataset} \
   --data_dir data/phase1_main/DATA \
+  --phase ${phase} \
   --spk_embed_dir data/spk_embed \
   --tag ${tag} \
   --ep ${ep} \
   --mode ${mode} || exit 1
 
-./pack_for_voicemos.sh ${tag}.zip exp/${tag}/${dataset}_${mode}_valid/${ep}_answer.txt
+./pack_for_voicemos.sh ${tag}.zip exp/${tag}/${dataset}_${mode}_${phase}/${ep}_answer.txt
